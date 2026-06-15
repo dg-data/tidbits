e75f5b315bd63d0e85d0413e7127a62df8c4823a
dg-data <66296000+dg-data@users.noreply.github.com>
2026-06-15 08:55:37 +0200

    Update import.js
    error handling

--- import.js
+++ import.js
@@ -6,7 +6,7 @@ function download(url, dest) {
     return new Promise((resolve, reject) => {
       const request = https.get(url, response => {
         if (response.statusCode === 200) {
-          const file = fs.createWriteStream(dest, { flags: 'wx' });
+          const file = fs.createWriteStream(dest, { flags: 'w' });
           file.on('finish', () => resolve());
           file.on('error', err => {
             file.close();
@@ -31,3 +31,4 @@ function download(url, dest) {
 var url = 'https://my.microsoftpersonalcontent.com/personal/b4e529b179b5976d/_layouts/15/download.aspx?share=' +
     (lang === 'hu' ? 'IQQKO54HqiNURaA0mqDd91lZAd5o-JhwFzbp23zH7pDs0_4' : 'F1A01879C77A02B3&resid=F1A01879C77A02B3%21107&authkey=APIgto_m31k5He0')
 download(url, './_data/me.yml')
+    .catch(err => { console.error('Downloading content from drive failed:', err); process.exit(1); });
